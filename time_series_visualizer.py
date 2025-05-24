63ca12bc476132b1a82a34f8537bcb2712b4f557
JhansiRaniMogalthuru <jhansir0903@gmail.com>
2025-05-24 16:03:29 +0530

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def draw_line_plot():
+    df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
+    df = df[(df['value'] >= df['value'].quantile(0.025)) &
+            (df['value'] <= df['value'].quantile(0.975))]
+
+    fig, ax = plt.subplots(figsize=(15, 5))
+    ax.plot(df.index, df['value'], 'r', linewidth=1)
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+    fig.savefig('line_plot.png')
+    return fig
 
