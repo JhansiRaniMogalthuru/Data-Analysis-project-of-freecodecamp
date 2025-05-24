b6336decd4881351c8eec77eacdca2eb4c9d218a
JhansiRaniMogalthuru <jhansir0903@gmail.com>
2025-05-24 16:01:22 +0530

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,1 +1,24 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def draw_cat_plot():
+    df = pd.read_csv('medical_examination.csv')
+
+    df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2 > 25).astype(int)
+
+    df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+    df['gluc'] = (df['gluc'] > 1).astype(int)
+
+    df_cat = pd.melt(df,
+                     id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+
+    fig = sns.catplot(data=df_cat, kind='bar', x='variable', y='total', hue='value', col='cardio')
+    fig.set_axis_labels("variable", "total")
+
+    fig.savefig('catplot.png')
+    return fig.fig
 
